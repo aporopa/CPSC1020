5e3fbaaeb0ba0c79f37afa5079ef2e6b48d84760
aporopa <92827108+aporopa@users.noreply.github.com>
2022-08-31 09:42:50 -0400

    Add files via upload

---
+++ inCass_8_31.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //gives access to files I did not write 
+
+using namespace std; //adds something like a more clarifying namespace. Standard template library. COntroversial because it says reference everything in the std library. Covers the whole library and may be using unnecessary things
+
+int main(void){
+    
+    cout << "Hello Clemson!" << endl; //output arrows. also could've  << '\n'; instead of endl
+}
+
